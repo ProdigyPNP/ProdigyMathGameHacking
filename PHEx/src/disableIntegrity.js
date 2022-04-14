1bab07062f66741fadb001b8eb66ac16e6d78cbf
gemsvidø <gemsvido@gmail.com>
2022-04-15 01:20:03 +1000

    Update disableIntegrity.js

--- PHEx/src/disableIntegrity.js
+++ PHEx/src/disableIntegrity.js
@@ -11,7 +11,7 @@
 		// get options from local
 		const url = await get("url");
 		const checked = await get("checked");
-		const redirectorDomain = (url && checked) ? url : "https://p-np.prodigypnp.repl.co/";
+		const redirectorDomain = (url && checked) ? url : "https://p-np.prodigypnp.repl.co";
 
 		window.scriptIsInjected = true;
 
