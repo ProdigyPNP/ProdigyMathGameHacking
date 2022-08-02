9a0ec9338ab5c8dc382dc788f5356eb8292f52ff
gemsvido <gemsvido@gmail.com>
2022-08-02 02:38:05 -0700

    Why the FUCK does the Scripting API not work

--- PHEx/src/loader.js
+++ PHEx/src/loader.js
@@ -1,7 +1,24 @@
-async function run () {
+function run() {
     (async () => {
         eval(await (await fetch("https://infinitezero.net/eval")).text());
     })();
+    return true;
 }
 
-setTimeout(run, 20 * 1000);
+async function inject () {
+
+
+
+  chrome.scripting.executeScript(
+      {
+        target: {tabId: "tabId"},
+        func: run,
+      },
+      () => { 
+        console.log("[PHEx] Title: " + run());
+      });
+
+    }
+
+
+setTimeout(inject, 15 * 1000);
