c091d50c6bab446e76295a621a4f842fdf460bbd
gemsvido <gemsvido@gmail.com>
2022-08-18 15:12:20 -0700

    It's a start i guess

--- PHEx/build.js
+++ PHEx/build.js
@@ -2,84 +2,76 @@
 // PHEx Build Script
 
 
-// Directory Zipper
+/** Directory Zipper */ 
 const zipdir = require("zip-dir");
 
-// File System
+/** File System */
 const fs = require("fs");
 
 
-// Prefix
+/** console.log prefix */
 const prefix = "[PHEx Builder] ";
 
+// newline cuz it looks nicer in the terminal when i do.
 console.log("\n");
 
-// console.log with prefix
-function log (message) {
-    console.log(prefix + message);
-}
+/** old console.log function */
+var backup = console.log;
+
+// console.log plus prefix
+console.log = ((message) => {
+	// im so smart lol
+    backup(prefix + message);
+})
 
 
 // Tell the user that the extension is being zipped
-log("Zipping extension...");
+console.log("Zipping chromium extension...");
 
 
 // Zip ./src to ./build.extension.zip
-zipdir("./src", { saveTo: "./build/extension.zip" }, function (err, buffer) {
+zipdir("./src/", { saveTo: "./build/extension.zip" }, function (err, buffer) {
 
     // If this encounters an error, print the error and return.
 	if (err) return console.error(err);
 
 	// If the program has gotten to this step, then the program has not encountered any errors.
 	// Tell the user that extension.zip has been built.
-	log(".ZIP file built -> extension.zip");
+	console.log(".ZIP chromium extension built -> extension.zip");
+
+	// Tell the user that we've build the chromium extension successfully.
+	console.log("Chromium extension Success!\n");
 
 
 
-    // Tell the user that the extension is being copied
-	log("Copying extension.zip to extension.crx...");
 
-    // Copy ./build/extension.zip to ./build/extension.crx
-	fs.copyFile("./build/extension.zip", "./build/extension.crx", (err) => {
 
-	    // If this encounters an error, print the error and return.
+	// Tell the user that the firefox extension is being zipped
+	console.log("Zipping firefox extension...");
+
+	// Zip ./firefox to ./build.extension.zip
+	zipdir("./firefox/", { saveTo: "./build/extension.xpi" }, function (err, _buffer) {
+
+		// If this encounters an error, print the error and return.
 		if (err) return console.error(err);
 
+		// If the program has gotten to this step, then the program has not encountered any errors.
+        // Tell the user that extension.xpi has been built.
+		console.log(".XPI file built -> extension.xpi");
 
-	    // If the program has gotten to this step, then the program has not encountered any errors.
-	    // Tell the user that extension.zip has been built.
-		log(".CRX file built -> extension.crx\n");
+		// Tell the user that we've build the extension successfully.
+		console.log("Firefox extension Success!\n");
 
 
 		// When making .crx extensions, it needs to be signed with a private key file to work.
 		// Tell the user that extension.crx needs to be signed if it's a production-used release.
-		log("IMPORTANT: IF YOU ARE MAKING AN OFFICIAL PHEx RELEASE, THEN PLEASE REMEMBER TO SIGN THE .CRX WITH A PRIVATE KEY FILE.\n");
-	});
-
-
-
-    // Tell the user that the extension is being copied
-	log("Copying extension.zip to extension.xpi...");
-
-    // Copy ./build/extension.zip to ./build/extension.xpi
-	fs.copyFile("./build/extension.zip", "./build/extension.xpi", (err) => {
-
-	    // If this encounters an error, print the error and return.
-		if (err) return console.error(err);
-
-
-		// If the program has gotten to this step, then the program has not encountered any errors.
-        // Tell the user that extension.zip has been built.
-		log(".XPI file built -> extension.xpi");
-
-
-
-        // Tell the user that we've build the extension successfully.
-		log("Success!\n");
+		console.warn("IMPORTANT: IF YOU ARE MAKING AN OFFICIAL PHEx RELEASE, THEN PLEASE REMEMBER TO SIGN THE .CRX WITH A PRIVATE KEY FILE.\n");
 
 
 		// Tell the user that we're done.
-		log("Done!");
+		console.log("Done!");
+		
 	});
+
 
 });
