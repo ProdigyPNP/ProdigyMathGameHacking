8be584a9bc9a13030b66ec16cc888b51470f1613
gemsvido <gemsvido@gmail.com>
2022-09-01 00:16:16 -0700

    worthwile

---
+++ cheatGUI/src/utils/divide.ts
@@ -0,0 +1,14 @@
+/** divide a string into parts */
+export function divide (target : string, amount : number) : Array<string> {
+
+    let output : Array<string> = new Array;
+    let parts : number = Math.floor(target.length / amount);
+    let start : number = 0;
+ 
+    while (start < target.length) {
+        output.push(target.substring(start, start+parts));
+        start += parts;
+    }
+
+    return output;
+}
