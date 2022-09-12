3e4cbc5726054aefc95cbb61e7b5ba27cb80fea4
gemsvido <gemsvido@gmail.com>
2022-09-12 16:19:53 -0700

    fix known bug

--- cheatGUI/src/utils/chat.ts
+++ cheatGUI/src/utils/chat.ts
@@ -1,22 +1,29 @@
 
 let chat : Window | null = null;
 
-export default function openChat () {
+export default function openChat () : void {
 
     if (chat === null) {
-        // @ts-expect-error
-        chat = window.open("https://chat.prodigypnp.com:8443/", null, `
-            height=800,
-            width=350,
-            status=yes,
-            toolbar=no,
-            menubar=no,
-            location=no
-        `);
+        open()
         // chat?.document.body.setAttribute("style", "zoom: 66%; height: 800px; width: 350px;");
     } else {
-        chat.focus()
+        if (chat.closed)
+            open()
+        else
+            chat.focus()
     }
     
 
+}
+
+function open () : void {
+    // @ts-expect-error
+    chat = window.open("https://chat.prodigypnp.com:8443/", null, `
+        height=800,
+        width=350,
+        status=yes,
+        toolbar=no,
+        menubar=no,
+        location=no
+    `);
 }
