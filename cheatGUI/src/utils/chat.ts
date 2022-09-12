d1b6ac1213e4eb0cdfc5a35c4bbb9f22686ec7a9
gemsvido <gemsvido@gmail.com>
2022-09-11 23:14:51 -0700

    this does N O T H I N G

--- cheatGUI/src/utils/chat.ts
+++ cheatGUI/src/utils/chat.ts
@@ -1,3 +1,12 @@
+import { category } from "../index";
+
 export default function addChat () {
     
+    let chatFrame = document.createElement("iframe");
+    chatFrame.setAttribute("src", "https://chat.prodigypnp.com:8443/");
+    chatFrame.setAttribute("class", "chat-frame");
+    chatFrame.setAttribute("id", "chat-frame");
+
+    category.misc.append(chatFrame)
+
 }
